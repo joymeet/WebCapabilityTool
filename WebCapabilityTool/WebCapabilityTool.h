93a93e30098d27629b4c5eefa65c043d66fb36fd
lishuilong <lishuilongoffice@gmail.com>
2024-05-22 18:42:23 +0800

    Initial Commit

---
+++ WebCapabilityTool/WebCapabilityTool.h
@@ -0,0 +1,18 @@
+//
+//  WebCapabilityTool.h
+//  WebCapabilityTool
+//
+//  Created by LiShuilong on 2024/5/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebCapabilityTool.
+FOUNDATION_EXPORT double WebCapabilityToolVersionNumber;
+
+//! Project version string for WebCapabilityTool.
+FOUNDATION_EXPORT const unsigned char WebCapabilityToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebCapabilityTool/PublicHeader.h>
+
+
